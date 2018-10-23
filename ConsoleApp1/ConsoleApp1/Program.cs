51f1914beb3240b16ebbeb4c213c1446dffdf87d
Phan Quoc Thai <pq_thai@yahoo.com.vn>
2018-10-23 13:49:53 +0700

    8 Edit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -34,7 +34,7 @@ namespace TestDateTime
         }
 
 
-        static void Test_Two(string[] args)
+        static void Test_Two (string[] args)
         {
             DateTime dateTime1 = DateTime.Now.ToLocalTime();
             Console.WriteLine(dateTime1);
@@ -60,31 +60,6 @@ namespace TestDateTime
         }
 
         static void Test_Ten(string[] args)
-        {
-            DateTime dateTime1 = DateTime.Now.ToLocalTime();
-            Console.WriteLine(dateTime1);
-            DateTime dateTime2 = dateTime1.AddDays(2);
-            Console.WriteLine(dateTime2);
-            DateTime dateTime3 = dateTime2.AddHours(5);
-            Console.WriteLine(dateTime3);
-
-            DateTime dateTime4 = DateTime.UtcNow.Date;
-            for (int i = 0; i < 10; i++)
-            {
-                //DateTime dateTime1 = DateTime.Now.ToLocalTime();
-
-                for (int j = 0; j < 24; j++)
-                {
-                    Console.WriteLine(dateTime4);
-                    dateTime4 = dateTime4.AddHours(1);
-
-                }
-                dateTime4 = dateTime4.AddDays(-2).Date;
-                Console.WriteLine(dateTime4);
-            }
-        }
-
-        static void Test_Six(string[] args)
         {
             DateTime dateTime1 = DateTime.Now.ToLocalTime();
             Console.WriteLine(dateTime1);
